cbe8e6153d81bf7b51787189417ab6793a11bf36
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 11:55:52 +0200

    created a user class model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from neighbourhood.models import Neighbourhood
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField(upload_to='user_avatars', default='default.jpg')
+    bio = models.TextField(blank=True, default='')
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='residents', blank=True, null=True, on_delete=models.SET_NULL)
+
+
+    def get_absolute_url(self):
+        return reverse('profile', args=[self.id])
