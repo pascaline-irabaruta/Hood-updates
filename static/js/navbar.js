14f426a2b3364929418c9c785745160267eb9788
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 16:27:13 +0200

    added jquery to the navigation bar

---
+++ static/js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".navbar-burger").click(function () {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    $('#tab_header ul li.item').on('click', function () {
+        var number = $(this).data('option');
+        $('#tab_header ul li.item').removeClass('is-active');
+        $(this).addClass('is-active');
+        $('#tab_container .container_item').removeClass('is-active');
+        $('div[data-item="' + number + '"]').addClass('is-active');
+    });
+
+
+    $('#business_search').submit(function (e) {
+        e.preventDefault();
+    });
+});
