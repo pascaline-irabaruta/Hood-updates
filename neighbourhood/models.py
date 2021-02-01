95a9197dea351d46564dded71327f3be1009dd75
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 15:34:44 +0200

    created the neighbourhood model class

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Neighbourhood(models.Model):
+    image = models.ImageField(upload_to='neighbourhood_avatars', default='dummy_neighbourhood.jpg')
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    police_hotline= ArrayField(models.CharField(max_length=13, blank=True),size=3, blank=True, null=True)
+    hospital_hotline= ArrayField(models.CharField(max_length=13, blank=True),size=3, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
