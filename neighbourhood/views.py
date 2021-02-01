f494e7752f137af70e8e0e90a296b2a21fba7637
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 15:38:39 +0200

    added a neighbourhood detail view

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView, CreateView
+from django.shortcuts import redirect
+from .models import Neighbourhood, Business
 
 # Create your views here.
+
+
+class NeighbourhoodDetailView(LoginRequiredMixin,DetailView):
+    model = Neighbourhood
+    template_name = 'neighbourhood.html'
+    login_url = 'login'
+
+    def dispatch(self, request, *args, **kwargs): # new
+        return super().dispatch(request, *args, **kwargs)
+
+class NeighbourhoodListView(LoginRequiredMixin,ListView):
+    model = Neighbourhood
+    template_name = 'neighbourhood_list.html'
+    login_url = 'login'
