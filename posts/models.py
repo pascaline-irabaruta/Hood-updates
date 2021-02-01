e64fac4bf527570c9b56d04a4e4ca27e0cf0a634
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 15:51:59 +0200

    created a tags model class

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from neighbourhood.models import Neighbourhood
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your models here.
+class Tags(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
