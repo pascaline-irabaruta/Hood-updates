a3008a2858d8ca7803a13a952f1ed00e59bcad45
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 11:54:28 +0200

    created a class for user form

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'avatar', 'email')
