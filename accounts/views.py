09f0bb94f18924c018df354ff12fe8b5c7411fbe
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 11:59:17 +0200

    created a user registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
-
+from django.views.generic import CreateView, DetailView, UpdateView
+from django.urls import reverse_lazy
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+
+class UserRegistrationView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration.html'
+    success_url = reverse_lazy('login')
