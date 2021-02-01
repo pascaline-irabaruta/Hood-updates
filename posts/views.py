e18ca936656c2508534df81272a840dc97eaca6e
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 15:53:58 +0200

    created a view for creating a post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Post
 
 # Create your views here.
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ('title', 'body', 'tags')
+    login_url = 'login'
+
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        form.instance.neighbourhood = self.request.user.neighbourhood
+        return super().form_valid(form)
