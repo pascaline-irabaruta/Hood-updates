a3c9f88bf3fd09a6594299771806e41df1651725
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-01 11:57:44 +0200

    created an admin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('avatar','bio', 'neighbourhood')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('avatar','bio')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
